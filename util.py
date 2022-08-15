8ce5f2ead648dbb491300cdea0cbfb72b09bc5d2
Edward Chen <edwjchen@gmail.com>
2022-08-15 13:24:14 -0400

    biomatch wan lan tests

--- util.py
+++ util.py
@@ -45,7 +45,7 @@ COSTS = HYCC_SOURCE+"/src/circuit-utils/py/costs.json"
 
 # hycc parameters
 HYCC_TEST_CASES = [
-    # ("biomatch", "biomatch/biomatch.c"),
+    ("biomatch", "biomatch/biomatch.c"),
     # ("kmeans", "kmeans/kmeans.c"),
     # ("gauss", "gauss/gauss.c"),
     # ("db_join", "db/db_join.c"),
@@ -53,7 +53,7 @@ HYCC_TEST_CASES = [
     # ("db_merge", "db/db_merge.c"),
     # ("mnist", "mnist/mnist.c"),
     # ("mnist_decomp_main", "mnist/mnist_decomp_main.c"),
-    ("mnist_decomp_convolution", "mnist/mnist_decomp_convolution.c"),
+    # ("mnist_decomp_convolution", "mnist/mnist_decomp_convolution.c"),
     # ("cryptonets", "cryptonets/cryptonets.c"),
     # ("histogram", "histogram/histogram.c"),
     # ("gcd", "gcd/gcd.c"),
