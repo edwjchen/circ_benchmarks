8b8369bcdc5697640a893f5ea2f92f91e5509cf1
Edward Chen <edwjchen@gmail.com>
2022-08-16 04:25:17 -0400

    mnist yao hybrid

--- util.py
+++ util.py
@@ -51,18 +51,18 @@ HYCC_TEST_CASES = [
     # ("db_join", "db/db_join.c"),
     # ("db_join2", "db/db_join2.c"),
     # ("db_merge", "db/db_merge.c"),
-    # ("mnist", "mnist/mnist.c"),
+    ("mnist", "mnist/mnist.c"),
     # ("mnist_decomp_main", "mnist/mnist_decomp_main.c"),
     # ("mnist_decomp_convolution", "mnist/mnist_decomp_convolution.c"),
-    ("cryptonets", "cryptonets/cryptonets.c"),
+    # ("cryptonets", "cryptonets/cryptonets.c"),
     # ("histogram", "histogram/histogram.c"),
     # ("gcd", "gcd/gcd.c"),
 ]
 
-MINIMIZATION_TIMES = [600]
+MINIMIZATION_TIMES = [0, 600]
 HYCC_SELECTION_SCHEMES = [
-    "yaoonly",
-    # "yaohybrid",
+    # "yaoonly",
+    "yaohybrid",
     # "gmwonly",
     # "gmwhybrid",
     # "hycc_optimized",
