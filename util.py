9ee078975a3bbe09ca3ef1f99f68a4b00f440437
Edward Chen <edwjchen@gmail.com>
2022-08-16 20:41:02 -0400

    recompile test cases

--- util.py
+++ util.py
@@ -48,18 +48,20 @@ HYCC_TEST_CASES = [
     # ("biomatch", "biomatch/biomatch.c"),
     # ("kmeans", "kmeans/kmeans.c"),
     # ("gauss", "gauss/gauss.c"),
-    # ("db_join", "db/db_join.c"),
-    # ("db_join2", "db/db_join2.c"),
-    # ("db_merge", "db/db_merge.c"),
-    # ("mnist", "mnist/mnist.c"),
-    # ("mnist_decomp_main", "mnist/mnist_decomp_main.c"),
+    ("db_join", "db/db_join.c"),
+    ("db_join2", "db/db_join2.c"),
+    ("db_merge", "db/db_merge.c"),
+    ("mnist", "mnist/mnist.c"),
+    ("mnist_decomp_main", "mnist/mnist_decomp_main.c"),
     # ("mnist_decomp_convolution", "mnist/mnist_decomp_convolution.c"),
     # ("cryptonets", "cryptonets/cryptonets.c"),
     # ("histogram", "histogram/histogram.c"),
-    ("gcd", "gcd/gcd.c"),
+    # ("gcd", "gcd/gcd.c"),
 ]
 
-MINIMIZATION_TIMES = [600]
+MINIMIZATION_TIMES = [
+    600
+]
 HYCC_SELECTION_SCHEMES = [
     # "yaoonly",
     # "yaohybrid",
