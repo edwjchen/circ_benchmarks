f1422f640ea149ebc24c880886c4676153fb731c
Edward Chen <edwjchen@gmail.com>
2022-08-17 02:40:14 -0400

    adding biomatch wan

--- util.py
+++ util.py
@@ -46,8 +46,8 @@ WAN_COSTS = CIRC_SOURCE+"/third_party/empirical_wan/adapted_costs.json"
 # hycc parameters
 HYCC_TEST_CASES = [
     # ("biomatch", "biomatch/biomatch.c"),
-    ("kmeans", "kmeans/kmeans.c"),
-    # ("gauss", "gauss/gauss.c"),
+    # ("kmeans", "kmeans/kmeans.c"),
+    ("gauss", "gauss/gauss.c"),
     # ("db_join", "db/db_join.c"),
     # ("db_join2", "db/db_join2.c"),
     # ("db_merge", "db/db_merge.c"),
@@ -60,7 +60,7 @@ HYCC_TEST_CASES = [
 ]
 
 MINIMIZATION_TIMES = [
-    600
+    9
 ]
 HYCC_SELECTION_SCHEMES = [
     # "yaoonly",
